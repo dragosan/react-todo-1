e8a828598ab9a7a704549aba1a3516501f2f0a9c
dragosan <donvader77@gmail.com>
2019-03-21 12:45:18 +0200

    Updates

---
+++ precache-manifest.0247c3876e64af819698aa257672b6b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2dfc619930aa4c6200f",
+    "url": "/react-todo-1/static/js/runtime~main.80db6158.js"
+  },
+  {
+    "revision": "3c457bcdd8cdfe2586aa",
+    "url": "/react-todo-1/static/js/main.33e6d2d4.chunk.js"
+  },
+  {
+    "revision": "5c4bf2425832fe4aa475",
+    "url": "/react-todo-1/static/js/2.76a70ea5.chunk.js"
+  },
+  {
+    "revision": "3c457bcdd8cdfe2586aa",
+    "url": "/react-todo-1/static/css/main.b18c061a.chunk.css"
+  },
+  {
+    "revision": "7a842dfaedce5a210f54bc2da4e61edb",
+    "url": "/react-todo-1/index.html"
+  }
+];
